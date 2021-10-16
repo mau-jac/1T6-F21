c4d7e9c0a0542a6094f8467c074422eb04fa1b28
Mauricio Buschinelli <mauricio.buschinelli@johnabbott.qc.ca>
2021-10-16 09:04:22 -0400

    better comments

--- wk8/writeFileSizes.sh
+++ wk8/writeFileSizes.sh
@@ -2,20 +2,23 @@
 
 # Print the size and name of all files inside the specified directory to a specified file inside the same directory
 
-# Store current path in a variable
+# Store current path into a variable 
 CURRENT_PATH=$pwd
 
-# Change directories to the path specified as the first argument
+# "$1" gives us the first argument passed to the script
 cd "$1"
 
 for file in *
 do
 	# Append to file specified in second argument
 	# file size, "comma", file name, line break
-	
+
+	# Stat command behaves diffently in MacOS, so...
 	if [[ "$OSTYPE" == "darwin"* ]]; then
+		# MacOS environment
 		stat -f "%z,%N" "$file" >> "$2"
 	else
+		# Git-bash or Linux
 		stat -c "%s,%n" "$file" >> "$2"
 	fi
 
